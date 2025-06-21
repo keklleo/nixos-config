eb85502ae2437719e894fbae33f324fc5bd0bb50
Leonhard Kost <leonhard.kost@gmail.com>
2025-06-21 15:45:50 +0200

    Add basic options and colorscheme to nixvim

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -3,6 +3,42 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
-    env = { EDITOR = "nvim"; };
+    vimdiffAlias = true;
+    defaultEditor = true;
+
+    colorscheme = "tokyonight";
+    colorschemes.tokyonight.enable = true;
+
+    opts = {
+      number = true;
+      relativenumber = true;
+      showmode = false;
+
+      breakindent = true;
+
+      undofile = true;
+
+      ignorecase = true;
+      smartcase = true;
+
+      signcolumn = "yes";
+
+      updatetime = 250;
+      timeoutlen = 300;
+
+      splitright = true;
+      splitbelow = true;
+
+      list = true;
+      listchars = { tab = "» "; trail = "·"; nbsp = "␣"; };
+
+      inccommand = "split";
+
+      cursorline = true;
+
+      scrolloff = 10;
+
+      confirm = true;
+    };
   };
 }
