8f6fe9e3037e68afc1026b6f29b4f017b86369ef
Leonhard Kost <leonhard.kost@gmail.com>
2025-12-15 23:08:41 +0100

    nixvim: use native neovim snippets

--- modules/home-manager/nixvim/plugins/cmp.nix
+++ modules/home-manager/nixvim/plugins/cmp.nix
@@ -17,7 +17,8 @@
         autoEnableSources = true;
 
         settings = {
-          snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+          snippet.expand = "function(args) vim.snippet.expand(args.body) end";
+
           formatting.fields = [
             "abbr"
             "kind"
@@ -34,17 +35,25 @@
             "<CR>" = "cmp.mapping.confirm({ select = true })";
             "<C-Space>" = "cmp.mapping.complete()";
             "<C-l>" = ''
-              cmp.mapping(function()
-                            if luasnip.expand_or_locally_jumpable() then
-                              luasnip.expand_or_jump()
-                            end
-                          end, {'i', 's'})'';
+              cmp.mapping(
+                function()
+                  if vim.snippet.active({ direction = 1 }) then
+                    vim.snippet.jump(1)
+                  end
+                end,
+                {'i', 's'}
+              )
+            '';
             "<C-h>" = ''
-              cmp.mapping(function()
-                            if luasnip.locally_jumpable(-1) then
-                              luasnip.jump(-1)
-                            end
-                          end, {'i', 's'})'';
+              cmp.mapping(
+                function()
+                  if vim.snippet.active({ direction = -1 }) then
+                    vim.snippet.jump(-1)
+                  end
+                end,
+                {'i', 's'}
+              )
+            '';
           };
 
           sources = [
