8c048ac3c2c2b0bcbea0bed18ce337550070a323
Leonhard Kost <leonhard.kost@gmail.com>
2025-12-15 14:59:45 +0100

    Use fish only as login shell and keep bash as system shell

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -1,9 +1,5 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.packages = with pkgs; [
-    fishPlugins.tide
-  ];
-
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
@@ -13,11 +9,19 @@
       set -g fish_key_bindings fish_vi_key_bindings
       # accept completion with ctrl-y
       bind --mode insert \cy forward-char
-      # initialize prompt
-      # tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time=No --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Few icons' --transient=Yes
     '';
     shellAbbrs = {
       la = "ls -lahH";
     };
+    plugins = with pkgs.fishPlugins; [
+      {
+        name = "tide";
+        inherit (tide) src;
+      }
+    ];
   };
+
+  home.activation.configure-tide = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ${pkgs.fish}/bin/fish -c "tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time=No --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Few icons' --transient=Yes"
+  '';
 }
