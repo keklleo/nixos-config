8c8c80c384bc6c3eb6aa7b565bebddeff2dcc602
Leonhard Kost <leonhard.kost@gmail.com>
2026-01-31 19:39:01 +0100

    feat: add git oauth -device flag for non graphical system

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   home.packages = with pkgs; [
     git-credential-oauth
@@ -11,7 +16,7 @@
       user.email = "leonhard.kost@gmail.com";
       credential.helper = [
         "cache --timeout 21600"
-        "oauth" # Eventually with optional -device flag
+        ("oauth" + lib.optionalString (!config.kekleo.graphical) " -device")
       ];
     };
   };
