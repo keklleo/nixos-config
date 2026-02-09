4fd953b18889363d63dad0db4996f1b958fd17cb
Leonhard Kost <leonhard.kost@gmail.com>
2026-02-09 11:04:37 +0100

    feat(agenix): use host keys for decryption

--- nixos/server/secrets/default.nix
+++ nixos/server/secrets/default.nix
@@ -1,15 +1,4 @@
 {
-  config,
-  pkgs,
-  ...
-}:
-let
-  keys = builtins.map (
-    key: builtins.toString (pkgs.writeText "agenix-key" key)
-  ) config.kekleo.publicKeys;
-in
-{
-  age.identityPaths = keys;
   age.secrets.nc_customer = {
     file = ./nc_customer.age;
     owner = "dyndns";
