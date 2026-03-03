af310c237ffc6be683f9e9c807686210c358b0b4
Leonhard Kost <leonhard.kost@gmail.com>
2026-03-03 12:51:24 +0100

    feat: xdg variables

--- nixos/base/default.nix
+++ nixos/base/default.nix
@@ -30,6 +30,13 @@
       nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
     };
 
+  environment.sessionVariables = {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+  };
+
   environment.systemPackages = with pkgs; [
     vim
     wget
